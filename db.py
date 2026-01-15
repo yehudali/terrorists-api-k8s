0b29f1e88403df7374884d1baf41cc563b2e0ac2
yehuda linker <yehuda.linker@gmail.com>
2026-01-15 13:56:17 +0200

    fix 2

--- db.py
+++ db.py
@@ -8,10 +8,10 @@ from pymongo import MongoClient
 
 class MongoDBConnector:
     def __init__(self) -> None:
-        self.mongo_host =os.getenv("HOST", "mongo-0.mongo")
-        self.mongo_port = int(os.getenv("PORT", 27017))
-        self.username = os.getenv("COLLECTION", "admin")
-        self.db_name = os.getenv("DB","threat_db")
+        self.mongo_host =os.getenv("MONGO_HOST", "mongo-0.mongo")
+        self.mongo_port = int(os.getenv("MONGO_PORT", 27017))
+        self.username = os.getenv("MONGO_USERNAME", "admin")
+        self.db_name = os.getenv("MONGO_DB","threat_db")
         self.mongo_auth_source = os.getenv("MONGO_AUTH_SOURCE","admin" )
 
         uri = f'mongodb://{self.mongo_host}:{int(self.mongo_port)}/'
@@ -28,18 +28,18 @@ class MongoDBInteractor:
     def save_most_5_dangerous(self,dangeres:dict) -> bool:
         print(self.db_connector.collection)
         try:
-            x = self.db_connector.collection.insert_many(dangeres)
+            x = self.db_connector.collection.insert_many([dangeres])
             print(f"saved?:{x}")
             return True
         except Exception as e:
             print(f"eror{e}")
             return False
 
-def get_5_most_dangerous_terrorist(df:DataFrame)->dict:
+def get_5_most_dangerous_terrorist(df:DataFrame)->DataFrame:
     df =  df.loc[:, ['name', 'location', 'danger_rate']]
     df = df.sort_values("danger_rate",ascending=False)
     df=df.loc[:4]
-    return df.to_dict()
+    return df
 
 
 
--- main.py
+++ main.py
@@ -16,14 +16,15 @@ def hello():
 @app.post("/top-threats")
 def foo(file: UploadFile):
     df = pd.read_csv(file.file)
-    dict_most_5_dangerous = get_5_most_dangerous_terrorist(df)
-    print(dict_most_5_dangerous)
+    df_most_5_dangerous = get_5_most_dangerous_terrorist(df)
+    print(df_most_5_dangerous)
+
     try:
-        saved=db.save_most_5_dangerous(dict_most_5_dangerous)
+        saved=db.save_most_5_dangerous(df_most_5_dangerous.to_dict())
         print(saved)
         if not saved:
             raise Exception("the: saved=db.save_most_5_dangerous(most_5_dangerous), not save ")
-        return {"saved":dict_most_5_dangerous}
+        return {"saved":df_most_5_dangerous.to_dict()}
 
     except Exception as e:
         print(f"eror:{e}")
